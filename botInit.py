78efbc2adfe1b220f3d080574015d5ea9c95fbc1
kindadeveloper <danilandryushenko34@gmail.com>
2020-06-15 16:41:39 +0200

    Genre search almost done

--- botInit.py
+++ botInit.py
@@ -15,8 +15,7 @@ headersIVA = {
         'x-rapidapi-key': "33e34af31fmsh45e531bff2a7970p1f7d96jsn6a0c3e75f69d",
         'content-type': "application/json"
         }
-responseBySearch = None
-responseByID = None
+nextPageToken = None
 userMessage = None
 pageMarker = 1
 bot = tgb.TeleBot('1107504191:AAFKdrsCNgf5rZLfIl0woHZVWD0VxYZ5FxU')
@@ -49,29 +48,69 @@ def startMessage(message):
     questionText = "чо хош?"
     bot.send_message(message.from_user.id, text=questionText, reply_markup=keyboard)
 
-def pagination(message):
+def pagination(message, resp):
     print(message)
-    allTitles = responseBySearch["Search"]
+    global nextPageToken
+    normAPI = None
+    allTitles = None
+    totalResults = None
+    if "Search" in resp.keys():
+        normAPI = True
+        allTitles = resp["Search"]
+        totalResults = resp["totalResults"]
+    if "Hits" in resp.keys():
+        normAPI = False
+        allTitles = resp["Hits"]
+        totalResults = resp["Total"]
+        nextPageToken = resp["NextPageToken"]
     paginationKeys = types.InlineKeyboardMarkup(row_width=5)
     titlesList = f'Search results {(pageMarker-1)*10 + 1}-' \
-        f'{(pageMarker-1)*10 + len(allTitles)} of {responseBySearch["totalResults"]}\n'
+        f'{(pageMarker-1)*10 + len(allTitles)} of {totalResults}\n'
     keysRow1 = []
     keysRow2 = []
     navigationRow = []
     for title in allTitles:
         position = str(allTitles.index(title) + 1)
-        keyNum = types.InlineKeyboardButton(text=position, callback_data=position)
+        if normAPI:
+            callbackData = f'{position}@{resp["Search"][int(position) - 1]["imdbID"]}'
+        else:
+            callbackData = f'{position}@'
+        keyNum = types.InlineKeyboardButton(text=position, callback_data=callbackData)
         keysRow1.append(keyNum) if int(position) <= len(allTitles)/2 else keysRow2.append(keyNum)
-        titlesList += f'\n{position}. {title["Title"]} ({title["Year"]})'
+        if normAPI:
+            titlesList += f'\n{position}. {title["Title"]} ({title["Year"]})'
+        else:
+            titlesList += f'\n{position}. {title["Source"]["Title"]} ({title["Source"]["Year"]})'
     if pageMarker != 1:
-        navigationRow.append(types.InlineKeyboardButton(text="<=", callback_data="prevPage"))
-    if pageMarker*10 <= int(responseBySearch["totalResults"]):
-        navigationRow.append(types.InlineKeyboardButton(text="=>", callback_data="nextPage"))
+        navigationRow.append(types.InlineKeyboardButton(text="<=", callback_data=f'prevPage'))
+    if pageMarker*10 <= int(totalResults):
+        navigationRow.append(types.InlineKeyboardButton(text="=>", callback_data=f'nextPage'))
     paginationKeys.add(*keysRow1, *keysRow2, *navigationRow)
     if not message.from_user.is_bot:
         bot.send_message(message.chat.id, titlesList, reply_markup=paginationKeys)
     else:
         bot.edit_message_text(text=titlesList, chat_id=message.chat.id, message_id=message.message_id, reply_markup=paginationKeys)
+
+# обработчик кнопок
+@bot.callback_query_handler(func=lambda call: True)
+def callback_worker(call):
+    global pageMarker
+    if call.data == "searchByName":
+        bot.send_message(call.message.chat.id, 'вводи название')
+    elif call.data == "prevPage":
+        if nextPageToken == None:
+            pageMarker -= 1
+            makeRequestByName(call.message)
+        else:
+            makeRequestByGenre(call.message, nextPageToken)
+    elif call.data == "nextPage":
+        if nextPageToken == None:
+            pageMarker += 1
+            makeRequestByName(call.message)
+        else:
+            makeRequestByGenre(call.message, nextPageToken)
+    elif re.search('\d', call.data.split('@')[0]):
+        makeRequestByID(call.message, call.data.split('@')[1])
 
 '''------------------------------------------------------------------------------------------------
 Получение ТОП 250 фильмов по оценке зрителей с сайта. 
@@ -102,69 +141,53 @@ def get_titles_top(titlesID:list):
 
 '''
         
-        
-        
+
 # отправка постера с описанием
-def sendTitleByID(message):
-    posterCaption = f'{responseByID["Title"]} ({responseByID["Year"]})\n' \
-        f'\nRated: {responseByID["Rated"]}' \
-        f'\nReleased: {responseByID["Released"]}' \
-        f'\nRuntime: {responseByID["Runtime"]}' \
-        f'\nAction: {responseByID["Genre"]}' \
-        f'\nCast: {responseByID["Actors"]}\n' \
-        f'\n{responseByID["Plot"]}'
-    bot.send_photo(message.chat.id, responseByID["Poster"], posterCaption)
+def sendTitleByID(message, respID):
+    posterCaption = f'{respID["Title"]} ({respID["Year"]})\n' \
+        f'\nRated: {respID["Rated"]}' \
+        f'\nReleased: {respID["Released"]}' \
+        f'\nRuntime: {respID["Runtime"]}' \
+        f'\nAction: {respID["Genre"]}' \
+        f'\nCast: {respID["Actors"]}\n' \
+        f'\n{respID["Plot"]}'
+    bot.send_photo(message.chat.id, respID["Poster"], posterCaption)
 
 # выполнение и обработка запроса по жанру фильма
-def makeRequestByGenre(message):
+def makeRequestByGenre(message, token):
     requestCount("requestsPerMonth.txt")
     querystring = {"Genres": userMessage.text, "SortBy": "IvaRating", "ProgramTypes": "Movie"}
+    if token: querystring.update({"NextPageToken": token})
     response = requests.request("GET", URLIVA, headers=headersIVA, params=querystring)
     responseByGenre = json.loads(response.text)
     print(responseByGenre)
+    pagination(message, responseByGenre)
 
 # выполнение и обработка запроса по ID фильма
 def makeRequestByID(message, ID):
     requestCount("requestsPerDay.txt")
-    global responseByID
     querystring = {"i": ID, "r": "json"}
     response = requests.request("GET", URLIMDB, headers=headersIMDB, params=querystring)
     responseByID = json.loads(response.text)
     print(responseByID)
-    sendTitleByID(message)
-
-# обработчик кнопок
-@bot.callback_query_handler(func=lambda call: True)
-def callback_worker(call):
-    global pageMarker
-    if call.data == "searchByName":
-        bot.send_message(call.message.chat.id, 'вводи название')
-    elif call.data == "prevPage":
-        pageMarker -= 1
-        makeRequestByName(call.message)
-    elif call.data == "nextPage":
-        pageMarker += 1
-        makeRequestByName(call.message)
-    elif re.search('\d', call.data):
-        makeRequestByID(call.message, responseBySearch["Search"][int(call.data) - 1]["imdbID"])
+    sendTitleByID(message, responseByID)
 
 # выполнение и обработка запроса по названию фильма
 def makeRequestByName(message):
     requestCount("requestsPerDay.txt")
-    global responseBySearch
     querystring = {"page": str(pageMarker), "r": "json", "s": userMessage.text}
     response = requests.request("GET", URLIMDB, headers=headersIMDB, params=querystring)
-    responseBySearch = json.loads(response.text)
-    print(responseBySearch)
-    pagination(message)
+    responseByName = json.loads(response.text)
+    print(responseByName)
+    pagination(message, responseByName)
 
 # обработчик текстового сообщения от юзера
 @bot.message_handler(content_types=['text'])
 def getMessageText(message):
     global userMessage
     userMessage = message
-    # makeRequestByName(message)
-    makeRequestByGenre(message)
+    makeRequestByName(message)
+    # makeRequestByGenre(message, False)
 
 # проверка на наличие новых сообщений у сервера телеги
 bot.polling(none_stop=True, interval=0)
